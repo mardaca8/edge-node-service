665a7d36f9408a01284975e715f1c348bc7ca972
Arturas Kovtun <mardaca8@gmail.com>
2026-04-23 13:31:02 +0300

    created SensorProcessor and SensorReadings

--- src/EdgeNode/Program.cs
+++ src/EdgeNode/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<SensorProcessor>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapPost("/data", (SensorReading r, SensorProcessor p) =>
+     {
+         if (string.IsNullOrWhiteSpace(r.SensorId))
+             return Results.BadRequest(new { error = "sensor_id is required" });
+         return Results.Ok(p.Process(r));
+     });
+
+app.MapGet("/sensors", (SensorProcessor p) => Results.Ok(p.Snapshot()));
 
 app.Run();
